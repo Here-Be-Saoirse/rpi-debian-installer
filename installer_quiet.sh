0443cd7569c0eab0c7cd54e76aa0ab08cd24db6f
Saoirse Ó Catháin <saoirse@axmiom-networks.nz>
2020-03-31 13:56:40 +1300

    removed silent mode to determine just what is holding up apt (installer_quiet)

--- installer_quiet.sh
+++ installer_quiet.sh
@@ -160,11 +160,11 @@ chroot /mnt apt upgrade -yq >/dev/null
 printf ' done.\n'
 printf 'Installing additional packages: '
 
-chroot /mnt apt install sudo ssh curl wget dbus usbutils ca-certificates crda less fbset debconf-utils avahi-daemon fake-hwclock nfs-common apt-utils man-db pciutils ntfs-3g apt-listchanges -yq >/dev/null
+chroot /mnt apt install sudo ssh curl wget dbus usbutils ca-certificates crda less fbset debconf-utils avahi-daemon fake-hwclock nfs-common apt-utils man-db pciutils ntfs-3g apt-listchanges -yq
 printf ' done.\n'
 printf 'Installing firmware: '
 
-chroot /mnt apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek dhcpcd5 net-tools -yq >/dev/null
+chroot /mnt apt install wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek dhcpcd5 net-tools -yq
 
 printf ' done.\n'
 if [[ ${RUNMODE} = debug ]]; then
